cd4ce118afdc2d28f02c7a3dbe8506627d883b09
Dharmil Shah <dharmilshah476@gmail.com>
2023-03-16 23:42:17 +0530

    Formated

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,18 +4,18 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: "bearer " + TMDB_TOKEN,
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchData = async (url, params) => {
-    try {
-        const { response } = await axios.get(BASE_URL + url, {
-            headers,
-            params
-        })
-        return response;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
-}
+  try {
+    const { response } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
