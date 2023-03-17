734c50540b2dcff43ea6c2cd933d201c86ddf92b
Dharmil Shah <dharmilshah476@gmail.com>
2023-03-17 23:55:33 +0530

    Added components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
-import {fetchData} from "./utils/api";
+import { fetchData } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/HomeSlice";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
-  const { url } = useSelector((state) => state.home)
-  console.log(url);
+  const { url } = useSelector((state) => state.home);
+  // console.log(url);
   const dispatch = useDispatch();
 
   useEffect(() => {
     apiData();
-  },[]);
+  }, []);
 
   const apiData = () => {
     fetchData("/movie/popular").then((res) => {
@@ -20,10 +29,17 @@ function App() {
   };
 
   return (
-    <div style={{ color: "white" }} className="App">
-     App
-     {url?.total_pages}
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/:mediaType/:id" element={<Details/>}/>
+        <Route exact path="/search/:query" element={<SearchResult/>} />
+        <Route exact path="/explore/:mediaType" element={<Explore/>} />
+        <Route exact path="*" element={<PageNotFound/>} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './heroBanner.scss'
+
+function HeroBanner() {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
