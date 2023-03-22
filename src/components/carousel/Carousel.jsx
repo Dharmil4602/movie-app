4dff2fc9fad9a0a8f4200fce94738ef846164d4d
Dharmil Shah <dharmilshah476@gmail.com>
2023-03-22 23:26:24 +0530

    Added carousel component

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './carousel.scss'
+
+function Carousel() {
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,25 +1,25 @@
-import React, {useState} from 'react'
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
-import useFetch from '../../../hooks/useFetch'
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
-
+import React, { useState } from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import useFetch from "../../../hooks/useFetch";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import Carousel from "../../../components/carousel/Carousel";
 
 function Trending() {
-    const [endPoint, setEndPoint] = useState("day")
-    const { data, loading } = useFetch(`/trending/all/${endPoint}`)
-    const onTabChange = (tab) => {
-        setEndPoint(tab === "Day" ? "day" : "week");
-    }
+  const [endPoint, setEndPoint] = useState("day");
+  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+  const onTabChange = (tab) => {
+    setEndPoint(tab === "Day" ? "day" : "week");
+  };
   return (
-    <div className='carouselSection'>
-        <ContentWrapper>
-            <span className="carouselTitle">Trending</span>
-            {/* The below switch tabs component is taking in an array of strings as props. This array is a dynamic array that will be passed in from the parent component. The array will be used to dynamically generate the tabs. In future we can add more tabs to the array and the component will automatically generate the tabs. Like "Day", "Week", "Month", "Year", "All Time". */}
-            <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange}/> 
-            
-        </ContentWrapper>
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        {/* The below switch tabs component is taking in an array of strings as props. This array is a dynamic array that will be passed in from the parent component. The array will be used to dynamically generate the tabs. In future we can add more tabs to the array and the component will automatically generate the tabs. Like "Day", "Week", "Month", "Year", "All Time". */}
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel/>
     </div>
-  )
+  );
 }
 
-export default Trending
+export default Trending;
