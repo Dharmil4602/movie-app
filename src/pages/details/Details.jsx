337cb6c62b125b3e86f2eb9ce3919ac6e954ddfb
Dharmil Shah <dharmilshah476@gmail.com>
2023-03-24 21:44:33 +0530

    Added new, Details banner component

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import './details.scss'
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+ 
 function Details() {
+  // const {mediaType, id} = useParams();
+  // const {data, loading} = useFetch(`/${mediaType}/${id}`)
   return (
     <div>Details</div>
   )
