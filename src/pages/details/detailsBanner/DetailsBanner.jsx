745c72361df206108f14ab79459d26e0cc1f42c5
Dharmil Shah <dharmilshah476@gmail.com>
2023-03-28 15:11:15 +0530

    Completed Detailsbanner

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -21,6 +21,11 @@ function DetailsBanner(props) {
     return `${hours}h${minutes > 0 ? ` ${minutes}` : ""}m;`;
   };
   const _genres = data?.genres?.map((g) => g.id);
+  const director = props.crew?.filter((f) => f.job === "Director"); // This will return an array of directors
+  const writer = props.crew?.filter(
+    (f) => f.job === "Screenplay" || f.job === "Story" || f.job === "Writer"
+  ); // This will return an array of writers
+
   return (
     <div className="detailsBanner">
       {!loading ? (
@@ -86,6 +91,48 @@ function DetailsBanner(props) {
                         </div>
                       )}
                     </div>
+
+                    {director?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Director: </span>
+                        <span className="text">
+                          {director.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {director.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Writer: </span>
+                        <span className="text">
+                          {writer.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {writer.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator: </span>
+                        <span className="text">
+                          {data?.created_by.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {data?.created_by.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
               </ContentWrapper>
